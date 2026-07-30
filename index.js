d320645adc026c7bb40c8b61db101599fb721730
itsamar971 <its.amarnadh123@gmail.com>
2026-07-30 08:46:19 +0530

    fix: add root index.js entrypoint for render

---
+++ index.js
@@ -0,0 +1,3 @@
+// This file acts as a fallback entrypoint for hosting platforms (like Render) 
+// that might be hardcoded to run `node index.js` in their Start Command settings.
+require('./dist/index.js');
